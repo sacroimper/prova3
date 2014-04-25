7f7010bbe766d8919b980e1c6adcec55a4000909
Joaquim Dalmau Torvà <jdalmaut@gmail.com>
2014-04-25 19:43:55 +0200

    Add method addProduct, removeProduct, getProduct, hasInProduct,iteratorProduct, sizeProduct

--- ARcowabungaproject-model/src/org/escoladeltreball/arcowabungaproject/model/ShoppingCart.java
+++ ARcowabungaproject-model/src/org/escoladeltreball/arcowabungaproject/model/ShoppingCart.java
@@ -1,10 +1,93 @@
 package org.escoladeltreball.arcowabungaproject.model;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class ShoppingCart extends IdObject {
+
+    // ====================
+    // CONSTANTS
+    // ====================
+
+    // ====================
+    // ATTRIBUTES
+    // ====================
+    private List<Product> productList;
+
+    // ====================
+    // CONSTRUCTORS
+    // ====================
 
     public ShoppingCart(int id) {
 	super(id);
-
     }
+
+    // ====================
+    // PUBLIC METHODS
+    // ====================
+    public boolean addProduct(final Product product) {
+	if (product != null) {
+	    if (productList == null) {
+		productList = new ArrayList<Product>();
+	    }
+	    return productList.add(product);
+	}
+	return false;
+    }
+
+    public boolean removeProduct(final Product product) {
+	if (product != null) {
+	    Iterator<Product> it = iteratorProducts();
+	    while (it.hasNext()) {
+		Product p = (Product) it.next();
+		if (p.equals(product)) {
+		    productList.remove(product);
+		    return true;
+		}
+	    }
+	}
+	return false;
+    }
+
+    public List<Product> getProduct() {
+	return productList;
+    }
+
+    public boolean hasInProducts(Product product) {
+	return productList.contains(product);
+    }
+
+    public Iterator<Product> iteratorProducts() {
+	return productList.iterator();
+    }
+
+    public int sizeProducts() {
+	return productList.size();
+    }
+
+    // public boolean removeAll() {
+    // productList.clear();
+    // }
+
+    // public void removeYou() {
+    //
+    // }
+
+    // ====================
+    // PROTECTED METHODS
+    // ====================
+
+    // ====================
+    // PRIVATE METHODS
+    // ====================
+
+    // ====================
+    // OVERRIDE METHODS
+    // ====================
+
+    // ====================
+    // GETTERS & SETTERS
+    // ====================
 
 }
