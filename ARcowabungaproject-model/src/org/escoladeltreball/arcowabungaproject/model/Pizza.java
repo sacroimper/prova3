2f125c9938a7900fac8bc627d6f4cac3a44b7896
Joaquim Dalmau Torvà <jdalmaut@gmail.com>
2014-04-26 19:41:03 +0200

    Add Pizza.java class

--- ARcowabungaproject-model/src/org/escoladeltreball/arcowabungaproject/model/Pizza.java
+++ ARcowabungaproject-model/src/org/escoladeltreball/arcowabungaproject/model/Pizza.java
@@ -1,5 +1,48 @@
 package org.escoladeltreball.arcowabungaproject.model;
 
 public class Pizza extends Product {
+    // ====================
+    // CONSTANTS
+    // ====================
+
+    // ====================
+    // ATTRIBUTES
+    // ====================
+    private String massType;
+    private String type;
+    private int size;
+
+    // private IngredientList ingredientList;
+
+    // ====================
+    // CONSTRUCTORS
+    // ====================
+    public Pizza(int id, String name, float price, int icon, float discount,
+	    String massType, String type, int size) {
+	super(id, name, price, icon, discount);
+	this.massType = massType;
+	this.type = type;
+	this.size = size;
+    }
+
+    // ====================
+    // PUBLIC METHODS
+    // ====================
+
+    // ====================
+    // PROTECTED METHODS
+    // ====================
+
+    // ====================
+    // PRIVATE METHODS
+    // ====================
+
+    // ====================
+    // OVERRIDE METHODS
+    // ====================
+
+    // ====================
+    // GETTERS & SETTERS
+    // ====================
 
 }
