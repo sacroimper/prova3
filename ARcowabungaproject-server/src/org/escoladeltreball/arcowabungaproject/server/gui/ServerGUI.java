5dd1d90a43a1d4cb0ffec6bb49a7820b4023c151
Marc Sabate <masapim@hotmail.com>
2014-04-27 20:52:38 +0200

    Add server project

---
+++ ARcowabungaproject-server/src/org/escoladeltreball/arcowabungaproject/server/gui/ServerGUI.java
@@ -0,0 +1,15 @@
+package org.escoladeltreball.arcowabungaproject.server.gui;
+
+import javax.swing.JFrame;
+
+public class ServerGUI extends JFrame {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
+
+    }
+
+}
