5e7426c01755e8ffa732b1e5b435fe193489edd9
Bernabe Gonzalez <bernagonzga@gmail.com>
2014-04-27 13:54:43 +0200

    Add new offer class to model.

---
+++ ARcowabungaproject-model/src/org/escoladeltreball/arcowabungaproject/model/Offer.java
@@ -0,0 +1,84 @@
+package org.escoladeltreball.arcowabungaproject.model;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Offer {
+    // ====================
+    // CONSTANTS
+    // ====================
+
+    // ====================
+    // ATTRIBUTES
+    // ====================
+    private List<Product> productList;
+
+    // ====================
+    // CONSTRUCTORS
+    // ====================
+
+    public Offer() {
+	super();
+	this.productList = new ArrayList<>();
+    }
+
+    // ====================
+    // PUBLIC METHODS
+    // ====================
+    public boolean addProduct(Product product) {
+	return this.productList.add(product);
+    }
+
+    public boolean removeProduct(Product product) {
+	return this.productList.remove(product);
+    }
+
+    public List<Product> getProduct() {
+	return this.productList;
+    }
+
+    public boolean hasInProducts(Product product) {
+	return this.productList.contains(product);
+    }
+
+    public Iterator<Product> iteratorProducts() {
+	return this.productList.iterator();
+    }
+
+    public int sizeProducts() {
+	return this.productList.size();
+    }
+
+    public boolean removeAll() {
+	return false;
+    }
+
+    public boolean removeYou() {
+	return false;
+    }
+
+    // ====================
+    // PROTECTED METHODS
+    // ====================
+
+    // ====================
+    // PRIVATE METHODS
+    // ====================
+
+    // ====================
+    // OVERRIDE METHODS
+    // ====================
+
+    // ====================
+    // ====================
+    // GETTERS & SETTERS
+
+    public List<Product> getProductList() {
+	return productList;
+    }
+
+    public void setProductList(List<Product> productList) {
+	this.productList = productList;
+    }
+}
