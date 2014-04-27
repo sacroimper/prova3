43a2df02aac284a52c1c1d6447d7459ffafb4e0f
Bernabe Gonzalez <bernagonzga@gmail.com>
2014-04-27 17:08:22 +0200

    Modify onCreate method, for hide the application tittle.

--- ARcowabungaproject/src/org/escoladeltreball/arcowabungaproject/MainActivity.java
+++ ARcowabungaproject/src/org/escoladeltreball/arcowabungaproject/MainActivity.java
@@ -1,22 +1,32 @@
 package org.escoladeltreball.arcowabungaproject;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+	// Remove title bar
+	this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+	// Remove notification bar
+	this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+		WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+	super.onCreate(savedInstanceState);
+	// set content view AFTER ABOVE sequence (to avoid crash)
+	this.setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+	// Inflate the menu; this adds items to the action bar if it is present.
+	getMenuInflater().inflate(R.menu.main, menu);
+	return true;
+    }
 
 }
