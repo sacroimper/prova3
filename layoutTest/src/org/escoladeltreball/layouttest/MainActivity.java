3acc51b775174d6dad2b2cc0d67f265e4a69b04a
Bernabe Gonzalez <bernagonzga@gmail.com>
2014-04-28 17:33:02 +0200

    Fix hide tittle on start layout.

--- layoutTest/src/org/escoladeltreball/layouttest/MainActivity.java
+++ layoutTest/src/org/escoladeltreball/layouttest/MainActivity.java
@@ -1,15 +1,26 @@
 package org.escoladeltreball.layouttest;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		// Remove title bar
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+		// Remove notification bar
+		this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+			WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+		super.onCreate(savedInstanceState);
+		// set content view AFTER ABOVE sequence (to avoid crash)
+		this.setContentView(R.layout.activity_main);
 	}
 
 	@Override
