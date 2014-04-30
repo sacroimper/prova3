c43d97cac9f11348e012ad2508c31af703995310
Marc Sabate <masapim@hotmail.com>
2014-04-30 18:44:34 +0200

    Change visibility of constants to public and add loadDemo method to DAOFactory class

--- ARcowabungaproject-model/src/org/escoladeltreball/arcowabungaproject/model/dao/DAOFactory.java
+++ ARcowabungaproject-model/src/org/escoladeltreball/arcowabungaproject/model/dao/DAOFactory.java
@@ -1,5 +1,6 @@
 package org.escoladeltreball.arcowabungaproject.model.dao;
 
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -13,66 +14,70 @@ import org.escoladeltreball.arcowabungaproject.model.ShoppingCart;
 import org.escoladeltreball.arcowabungaproject.model.system.Pizzeria;
 
 public abstract class DAOFactory {
+
     // ====================
     // CONSTANTS
     // ====================
-    private static final String CREATE_DATA_BASE = "CREATE DATABASE cowabunga;";
-    private static final String CREATE_TABLE_RESOURCES = "CREATE TABLE resources("
+
+    public static final String CREATE_DATA_BASE = "CREATE DATABASE cowabunga;";
+    public static final String CREATE_TABLE_RESOURCES = "CREATE TABLE resources("
 	    + "id_resources INTEGER PRIMARY KEY," + "path VARCHAR(150));";
-    private static final String CREATE_TABLE_INGREDIENT = "CREATE TABLE ingredient("
+    public static final String CREATE_TABLE_INGREDIENT = "CREATE TABLE ingredient("
 	    + "id_ingredient INTEGER PRIMARY KEY,"
 	    + "name VARCHAR(50), "
 	    + "icon SMALLINT FOREIGN KEY REFERENCES resources ON DELETE CASCADE ON UPDATE CASCADE,"
 	    + "model SMALLINT FOREIGN KEY REFERENCES resources ON DELETE CASCADE ON UPDATE CASCADE,"
 	    + "price NUMERIC);";
-    private static final String CREATE_TABLE_INGREDIENTS = "CREATE TABLE ingredients("
+    public static final String CREATE_TABLE_INGREDIENTS = "CREATE TABLE ingredients("
 	    + "id_ingredients INTEGER PRIMARY KEY,"
 	    + "ingredient INTEGER FOREIGN KEY REFERNECES ingredient ON DELETE CASCADE ON UPDATE CASCADE,"
 	    + "num_ingredients SMALLINT NOT NULL);";
-    private static final String CREATE_TABLE_PIZZAS = "CREATE TABLE pizzas("
+    public static final String CREATE_TABLE_PIZZAS = "CREATE TABLE pizzas("
 	    + "id_pizza INTEGER PRIMARY KEY,"
 	    + "name VARCHAR(50),"
 	    + "price NUMERIC,"
 	    + "icon SMALLINT FOREIGN KEY REFERNCES resources ON DELETE CASCADE ON UPDATE CASCADE,"
 	    + "massType VARCHAR(10)," + "type VARCHAR(10)," + "size SMALLINT);";
-    private static final String CREATE_TABLE_DRINKS = "CREATE TABLE drinks("
+    public static final String CREATE_TABLE_DRINKS = "CREATE TABLE drinks("
 	    + "id_drink INTEGER PRIMARY KEY,"
 	    + "name VARCHAR(50),"
 	    + "price NUMERIC,"
 	    + "icon SMALLINT FOREIGN KEY REFERENCES resources ON DELETE CASCADE ON UPDATE CASCADE,"
 	    + "discount NUMERIC," + "size SMALLINT);";
-    private static final String CREATE_TABLE_OFFERS = "CREATE TABLE offers("
+    public static final String CREATE_TABLE_OFFERS = "CREATE TABLE offers("
 	    + "id_offers INTEGER PRIMARY KEY,"
 	    + "name VARCHAR(30),"
 	    + "price NUMERIC,"
 	    + "icon SMALLINT FOREIGN KEY  REFERNCES resources ON DELETE CASCADE ON UPDATE CASCADE,"
 	    + "discount NUMERIC);";
-    private static final String CREATE_TABLE_OFFERS_PRODUCTS = "CREATE TABLE offers_products("
+    public static final String CREATE_TABLE_OFFERS_PRODUCTS = "CREATE TABLE offers_products("
 	    + "offer INTEGER FOREIGN KEY REFERENCES offers ON DELTE CASCADE ON UPDATE CASCADE,"
 	    + "pizza INTEGER FOREIGN KEY REFERNCES pizza ON DELTE CASCADE ON UPDATE CASCADE,"
 	    + "drink INTEGER FOREIGN KEY REFERNCES drink ON DELTE CASCADE ON UPDATE CASCADE"
 	    + ");";
-    private static final String CREATE_TABLE_SHOPPINGCARTS = "CREATE TABLE shoppingcarts(id_shoopingcart INTEGER PRIMARY KEY);";
-    private static final String CREATE_TABLE_SHOPPINCART_PRODUCTS = "CREATE TABLE shoopingcart_products("
+    public static final String CREATE_TABLE_SHOPPINGCARTS = "CREATE TABLE shoppingcarts(id_shoopingcart INTEGER PRIMARY KEY);";
+    public static final String CREATE_TABLE_SHOPPINCART_PRODUCTS = "CREATE TABLE shoopingcart_products("
 	    + "shoppincart INTEGER FOREIGN KEY REFERENCES shoppincart ON DELTE CASCADE ON UPDATE CASCADE "
 	    + "offer INTEGER FOREIGN KEY REFERENCES offers ON DELTE CASCADE ON UPDATE CASCADE,"
 	    + "pizza INTEGER FOREIGN KEY REFERNCES pizza ON DELTE CASCADE ON UPDATE CASCADE,"
 	    + "drink INTEGER FOREIGN KEY REFERNCES drink ON DELTE CASCADE ON UPDATE CASCADE);";
-    private static final String CREATE_TABLE_ORDERS = "CREATE TABLE orders("
+    public static final String CREATE_TABLE_ORDERS = "CREATE TABLE orders("
 	    + "id_order PRIMARY KEY,"
 	    + "email VARCHAR(50),"
 	    + "date_time DATE,"
 	    + "payment_method VARCHAR(15),"
 	    + "addres SMALLINT FOREIGN KEY REFERENCES addres ON DELTE CASCADE ON UPDATE CASCADE)";
-    private static final String CREATE_TABLE_ADDRES = "CREATE TABLE addres("
+    public static final String CREATE_TABLE_ADDRES = "CREATE TABLE addres("
 	    + "id_addres INTEGER PRIMARY KEY," + "number VARCHAR(3),"
 	    + "post_code VARCHAR(5)," + "floor VARCHAR(3)" + "stair CHAR,"
 	    + "door VARCHAR(2));";
-    private static final String CREATE_TABLE_PREFERENCES = "CREATE TABLE preferences(key VARCHAR(30) PRIMARY KEY,"
+    public static final String CREATE_TABLE_PREFERENCES = "CREATE TABLE preferences(key VARCHAR(30) PRIMARY KEY,"
 	    + "value VARCHAR(50));";
+
     // ====================
     // ATTRIBUTES
     // ====================
+
     private int currentVersion;
     protected static DAOFactory instance;
     private Pizzeria pizzeria;
@@ -80,6 +85,7 @@ public abstract class DAOFactory {
     // ====================
     // CONSTRUCTORS
     // ====================
+
     protected DAOFactory(Pizzeria pizzeria) {
 	this.pizzeria = pizzeria;
     }
@@ -91,6 +97,20 @@ public abstract class DAOFactory {
     public boolean loadData() {
 
 	return false;
+    }
+
+    public boolean loadDemo() {
+	Set<Pizza> predefinedPizzas = new HashSet<Pizza>();
+	Set<Pizza> customSavedPizzas = new HashSet<Pizza>();
+	Set<Pizza> customTemporaryPizzas = new HashSet<Pizza>();
+	Set<Order> ordersSaved = new HashSet<Order>();
+	Set<Ingredient> ingredients = new HashSet<Ingredient>();
+	Set<Drink> drinks = new HashSet<Drink>();
+	Set<Offer> offers = new HashSet<Offer>();
+
+	// Pizza p1 = new Pizza(1, "A", 10, 150, 0, massType, type, size)
+
+	return true;
     }
 
     public void writeDataBase() {
@@ -108,6 +128,7 @@ public abstract class DAOFactory {
     // ====================
     // PROTECTED METHODS
     // ====================
+
     protected abstract Set<Ingredient> readIngredient();
 
     protected abstract Set<Pizza> readPizza();
