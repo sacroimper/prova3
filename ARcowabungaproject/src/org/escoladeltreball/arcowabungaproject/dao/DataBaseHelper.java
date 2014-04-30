2af9e59d6f39d789ddde52690dd7f080ffbff174
Joaquim Dalmau Torvà <jdalmaut@gmail.com>
2014-04-30 19:48:34 +0200

    Implements onUpgrade method in DataBaseHelper.java

--- ARcowabungaproject/src/org/escoladeltreball/arcowabungaproject/dao/DataBaseHelper.java
+++ ARcowabungaproject/src/org/escoladeltreball/arcowabungaproject/dao/DataBaseHelper.java
@@ -4,18 +4,32 @@ import org.escoladeltreball.arcowabungaproject.model.dao.DAOFactory;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DataBaseHelper extends SQLiteOpenHelper{
+    //====================
+    // CONSTANTS
+    //====================
+
     private static final String DATABASE_NAME ="cowabunga.db";
     private static final int DATABASE_VERSION = 1;
     
-    
+    //====================
+    // ATTRIBUTES
+    //====================
+
+    //====================
+    // CONSTRUCTORS
+    //====================
+
     public DataBaseHelper(Context context) {
 	super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
+    //====================
+    // PUBLIC METHODS
+    //====================
+    
     @Override
     public void onCreate(SQLiteDatabase db) {
 	db.execSQL(DAOFactory.CREATE_TABLE_ADDRESS);
@@ -35,26 +49,23 @@ public class DataBaseHelper extends SQLiteOpenHelper{
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-	// TODO Auto-generated method stub
+	db.execSQL(DAOFactory.DROP_TABLE_ADDRESS);
+	db.execSQL(DAOFactory.DROP_TABLE_DRINKS);
+	db.execSQL(DAOFactory.DROP_TABLE_INGREDIENT);
+	db.execSQL(DAOFactory.DROP_TABLE_INGREDIENTS);
+	db.execSQL(DAOFactory.DROP_TABLE_OFFERS);
+	db.execSQL(DAOFactory.DROP_TABLE_OFFERS_PRODUCTS);
+	db.execSQL(DAOFactory.DROP_TABLE_ORDERS);
+	db.execSQL(DAOFactory.DROP_TABLE_PIZZAS);
+	db.execSQL(DAOFactory.DROP_TABLE_PREFERENCES);
+	db.execSQL(DAOFactory.DROP_TABLE_RESOURCES);
+	db.execSQL(DAOFactory.DROP_TABLE_SHOPPINGCARTS);
+	db.execSQL(DAOFactory.DROP_TABLE_SHOPPINGCARTS_PRODUCTS);
 	
     }
 
-    //====================
-    // CONSTANTS
-    //====================
-
-    //====================
-    // ATTRIBUTES
-    //====================
-
-    //====================
-    // CONSTRUCTORS
-    //====================
-
-    //====================
-    // PUBLIC METHODS
-    //====================
-
+   
+   
     //====================
     // PROTECTED METHODS
     //====================
