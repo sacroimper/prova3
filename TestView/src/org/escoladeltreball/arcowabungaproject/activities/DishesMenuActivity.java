4bcef5e28cef215c66821507b1b1a8466e044fa1
Victor Purcallas <vpurcallas@gmail.com>
2014-04-30 11:22:20 +0200

    Corrected a behaviour that changes the tab by clicking on the listviewEx pandible

--- TestView/src/org/escoladeltreball/arcowabungaproject/activities/DishesMenuActivity.java
+++ TestView/src/org/escoladeltreball/arcowabungaproject/activities/DishesMenuActivity.java
@@ -3,11 +3,8 @@ package org.escoladeltreball.arcowabungaproject.activities;
 import org.escoladeltreball.arcowabungaproject.R;
 import org.escoladeltreball.arcowabungaproject.adapters.Adaptador;
 import org.escoladeltreball.arcowabungaproject.model.GrupoDeItems;
-import org.escoladeltreball.arcowabungaproject.visualeffects.TabsSwipeAnimation;
-
 import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.SparseArray;
@@ -23,7 +20,6 @@ import android.widget.ExpandableListView;
 import android.widget.LinearLayout;
 import android.widget.TabHost;
 import android.widget.TextView;
-import android.widget.TabHost.OnTabChangeListener;
 import android.widget.TabHost.TabContentFactory;
 import android.widget.TabHost.TabSpec;
 
@@ -62,11 +58,12 @@ public class DishesMenuActivity extends Activity implements OnTouchListener {
 	ExpandableListView listView = (ExpandableListView) findViewById(R.id.listViewexp);
 	Adaptador adapter = new Adaptador(this, grupos);
 	listView.setAdapter(adapter);
-	// Para añadir swipe action al la Expandable list
-	listView.setOnTouchListener(this);
 
 	// Pestañas
 	makeTabs();
+
+	// Para añadir swipe action al la Expandable list
+	listView.setOnTouchListener(this);
 
     }
 
@@ -89,11 +86,14 @@ public class DishesMenuActivity extends Activity implements OnTouchListener {
 	spec.setContent(R.id.tab3);
 	spec.setIndicator("TAB3");
 	tabs.addTab(spec);
-	
-	//TabsColor
-	tabs.getTabWidget().getChildAt(0).setBackgroundColor(Color.parseColor("#3be0d0"));
-	tabs.getTabWidget().getChildAt(1).setBackgroundColor(Color.parseColor("#3be0d0"));
-	tabs.getTabWidget().getChildAt(2).setBackgroundColor(Color.parseColor("#3be0d0"));
+
+	// TabsColor
+	tabs.getTabWidget().getChildAt(0)
+		.setBackgroundColor(Color.parseColor("#3be0d0"));
+	tabs.getTabWidget().getChildAt(1)
+		.setBackgroundColor(Color.parseColor("#3be0d0"));
+	tabs.getTabWidget().getChildAt(2)
+		.setBackgroundColor(Color.parseColor("#3be0d0"));
 
 	int tabCount = tabs.getTabWidget().getTabCount();
 	for (int i = 0; i < tabCount; i++) {
@@ -121,44 +121,44 @@ public class DishesMenuActivity extends Activity implements OnTouchListener {
 	}
 	tabs.setCurrentTab(0);
 
-	actualTab = (LinearLayout) findViewById(R.id.tab1);
-	tabs.setOnTabChangedListener(new OnTabChangeListener() {
-
-	    LinearLayout t1l = (LinearLayout) findViewById(R.id.tab1);
-	    LinearLayout t2l = (LinearLayout) findViewById(R.id.tab2);
-	    LinearLayout t3l = (LinearLayout) findViewById(R.id.tab3);
-
-	    public void onTabChanged(String tabId) {
-		if (actualTab.equals(t1l) && tabId.equals("mitab2")) {
-		    t1l.setAnimation(TabsSwipeAnimation.outToLeftAnimation());
-		    t2l.setAnimation(TabsSwipeAnimation.inFromRightAnimation());
-		    actualTab = t2l;
-		} else if (actualTab.equals(t2l) && tabId.equals("mitab1")) {
-		    t2l.setAnimation(TabsSwipeAnimation.leftToLeftAnimation());
-		    t1l.setAnimation(TabsSwipeAnimation
-			    .leftFromRightAnimation());
-		    actualTab = t1l;
-		} else if (actualTab.equals(t2l) && tabId.equals("mitab3")) {
-		    t2l.setAnimation(TabsSwipeAnimation.outToLeftAnimation());
-		    t3l.setAnimation(TabsSwipeAnimation.inFromRightAnimation());
-		    actualTab = t3l;
-		} else if (actualTab.equals(t3l) && tabId.equals("mitab2")) {
-		    t3l.setAnimation(TabsSwipeAnimation.leftToLeftAnimation());
-		    t2l.setAnimation(TabsSwipeAnimation
-			    .leftFromRightAnimation());
-		    actualTab = t2l;
-		} else if (actualTab.equals(t1l) && tabId.equals("mitab3")) {
-		    t1l.setAnimation(TabsSwipeAnimation.outToLeftAnimation());
-		    t3l.setAnimation(TabsSwipeAnimation.inFromRightAnimation());
-		    actualTab = t3l;
-		} else if (actualTab.equals(t3l) && tabId.equals("mitab1")) {
-		    t3l.setAnimation(TabsSwipeAnimation.leftToLeftAnimation());
-		    t1l.setAnimation(TabsSwipeAnimation
-			    .leftFromRightAnimation());
-		    actualTab = t1l;
-		}
-	    }
-	});
+	// actualTab = (LinearLayout) findViewById(R.id.tab1);
+	// tabs.setOnTabChangedListener(new OnTabChangeListener() {
+	//
+	// LinearLayout t1l = (LinearLayout) findViewById(R.id.tab1);
+	// LinearLayout t2l = (LinearLayout) findViewById(R.id.tab2);
+	// LinearLayout t3l = (LinearLayout) findViewById(R.id.tab3);
+	//
+	// public void onTabChanged(String tabId) {
+	// if (actualTab.equals(t1l) && tabId.equals("mitab2")) {
+	// t1l.setAnimation(TabsSwipeAnimation.outToLeftAnimation());
+	// t2l.setAnimation(TabsSwipeAnimation.inFromRightAnimation());
+	// actualTab = t2l;
+	// } else if (actualTab.equals(t2l) && tabId.equals("mitab1")) {
+	// t2l.setAnimation(TabsSwipeAnimation.leftToLeftAnimation());
+	// t1l.setAnimation(TabsSwipeAnimation
+	// .leftFromRightAnimation());
+	// actualTab = t1l;
+	// } else if (actualTab.equals(t2l) && tabId.equals("mitab3")) {
+	// t2l.setAnimation(TabsSwipeAnimation.outToLeftAnimation());
+	// t3l.setAnimation(TabsSwipeAnimation.inFromRightAnimation());
+	// actualTab = t3l;
+	// } else if (actualTab.equals(t3l) && tabId.equals("mitab2")) {
+	// t3l.setAnimation(TabsSwipeAnimation.leftToLeftAnimation());
+	// t2l.setAnimation(TabsSwipeAnimation
+	// .leftFromRightAnimation());
+	// actualTab = t2l;
+	// } else if (actualTab.equals(t1l) && tabId.equals("mitab3")) {
+	// t1l.setAnimation(TabsSwipeAnimation.outToLeftAnimation());
+	// t3l.setAnimation(TabsSwipeAnimation.inFromRightAnimation());
+	// actualTab = t3l;
+	// } else if (actualTab.equals(t3l) && tabId.equals("mitab1")) {
+	// t3l.setAnimation(TabsSwipeAnimation.leftToLeftAnimation());
+	// t1l.setAnimation(TabsSwipeAnimation
+	// .leftFromRightAnimation());
+	// actualTab = t1l;
+	// }
+	// }
+	// });
     }
 
     /**
@@ -170,20 +170,15 @@ public class DishesMenuActivity extends Activity implements OnTouchListener {
 	GrupoDeItems grupo0 = new GrupoDeItems("Pizza Margarita");
 	grupo0.children
 		.add("Tomate, mozzarella, albahaca fresca, sal y aceite");
-	// grupo0.children.add("A la parrilla");
 	grupos.append(0, grupo0);
 
 	GrupoDeItems grupo1 = new GrupoDeItems("Pizza Caprichosa");
 	grupo1.children
 		.add("Tomate, mozzarella,alcachofas,champiñones, anchoas.");
-	// grupo1.children.add("A la parrilla");
-	// grupo1.children.add("Frito");
 	grupos.append(1, grupo1);
 
 	GrupoDeItems grupo2 = new GrupoDeItems("Pizza Cuatro Estaciones");
 	grupo2.children.add("Champiñones, alcachofa, jamon de york, aceitunas");
-	// grupo2.children.add("Pollo, morrones y aceitunas");
-	// grupo2.children.add("Carlitos");
 	grupos.append(2, grupo2);
 
 	GrupoDeItems grupo3 = new GrupoDeItems("Pizza Cuatro Quesos");
@@ -203,23 +198,23 @@ public class DishesMenuActivity extends Activity implements OnTouchListener {
 	grupos.append(6, grupo6);
     }
 
-    /**
-     * Move swipe touch acction
-     * 
-     * @param direction
-     */
-    public void switchTabs(boolean direction) {
-
-	if (direction) {
-	    if (tabs.getCurrentTab() != 0) {
-		tabs.setCurrentTab(tabs.getCurrentTab() - 1);
-	    }
-	} else {
-	    if (tabs.getCurrentTab() != (tabs.getTabWidget().getTabCount() - 1)) {
-		tabs.setCurrentTab(tabs.getCurrentTab() + 1);
-	    } 
-	}
-    }
+    // /**
+    // * Move swipe touch acction
+    // *
+    // * @param direction
+    // */
+    // public void switchTabs(boolean direction) {
+    //
+    // if (direction) {
+    // if (tabs.getCurrentTab() != 0) {
+    // tabs.setCurrentTab(tabs.getCurrentTab() - 1);
+    // }
+    // } else {
+    // if (tabs.getCurrentTab() != (tabs.getTabWidget().getTabCount() - 1)) {
+    // tabs.setCurrentTab(tabs.getCurrentTab() + 1);
+    // }
+    // }
+    // }
 
     // El onTouchEvent viene de serie con la Activity
     @Override
@@ -234,25 +229,23 @@ public class DishesMenuActivity extends Activity implements OnTouchListener {
 	return touchController(event);
     }
 
+    // Es necesario el Touch controller para que el evento de cambio
+    // Venga tambien desde el ListExpandable y su contenido
     private boolean touchController(MotionEvent event) {
 	switch (event.getAction()) {
 	// when user first touches the screen to swap
 	case MotionEvent.ACTION_DOWN: {
-	    lastX = event.getRawX();
+	    lastX = event.getX();
 	    break;
 	}
 	case MotionEvent.ACTION_UP: {
-	    float currentX = event.getRawX();
+	    float currentX = event.getX();
 	    // if left to right swipe on screen
-	    if (lastX < currentX + SCREEN_TOUCH_RESISTENCE) {
-
-		switchTabs(true);
+	    if (lastX < currentX - 250) {
+		tabs.setCurrentTab(tabs.getCurrentTab() - 1);
+	    } else if (lastX > currentX + 250) {
+		tabs.setCurrentTab(tabs.getCurrentTab() + 1);
 	    }
-	    // if right to left swipe on screen
-	    if (lastX > currentX - SCREEN_TOUCH_RESISTENCE) {
-		switchTabs(false);
-	    }
-
 	    break;
 	}
 	}
