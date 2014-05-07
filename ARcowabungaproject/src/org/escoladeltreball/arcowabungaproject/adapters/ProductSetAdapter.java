6ef4566e3cd2618c6cc91472ced3e3d85a026a04
Victor Purcallas <vpurcallas@gmail.com>
2014-05-07 12:17:46 +0200

    Advance the ProductSetAdapter.java class

--- ARcowabungaproject/src/org/escoladeltreball/arcowabungaproject/adapters/ProductSetAdapter.java
+++ ARcowabungaproject/src/org/escoladeltreball/arcowabungaproject/adapters/ProductSetAdapter.java
@@ -4,9 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
+import org.escoladeltreball.arcowabungaproject.R;
+import org.escoladeltreball.arcowabungaproject.dao.DAOAndroid;
 import org.escoladeltreball.arcowabungaproject.model.Product;
 
 import android.app.Activity;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -79,20 +82,61 @@ public class ProductSetAdapter extends BaseAdapter {
 	ViewHolder holder = null;
 
 	if (convertView == null) {
-	    // Se ha de añadir el layout
-	    // convertView = this.inflater.inflate(resource, root);
+	    convertView = this.inflater.inflate(
+		    R.layout.listitem_product_layout, parent, false);
 
+	    holder = new ViewHolder();
+
+	    holder.productImage = (ImageView) convertView
+		    .findViewById(R.id.imageInProductItem);
+	    holder.productTitle = (TextView) convertView
+		    .findViewById(R.id.titleTextInProductItem);
+	    holder.trashIcon = (ImageButton) convertView
+		    .findViewById(R.id.trashIcon);
+	    holder.productPrice = (TextView) convertView
+		    .findViewById(R.id.priceTextInProductItem);
+	    // Is necesary to develope this element to inflate the content of
+	    // this LinearLayout
+	    // Maybe it has another Adapter?
+	    holder.extraIngrentsLayout = (LinearLayout) convertView
+		    .findViewById(R.id.extraIngredientLayoutInProductItem);
+
+	    convertView.setTag(holder);
+
+	} else {
+	    holder = (ViewHolder) convertView.getTag();
 	}
-	return null;
+
+	Product product = this.products.get(position);
+
+	DAOAndroid dao = DAOAndroid.getInstance();
+	Drawable titleIcon = dao.getDrawableFromAssets(activity,
+		product.getIcon());
+
+	holder.productImage.setBackgroundDrawable(titleIcon);
+	holder.productTitle.setText(product.getName());
+
+	// IconPizza will be changed by trash icon when exists
+	Drawable trashIcon = dao.getDrawableFromAssets(activity,
+		R.drawable.iconpizza);
+	holder.productImage.setBackgroundDrawable(trashIcon);
+	holder.productPrice.setText(product.getFormatedPrice());
+
+	// It will be necessary to inflate this layout only if extra ingredients
+	// were added
+	// holder.extraIngrentsLayout.
+	// (R.id.extraIngredientLayoutInProductItem);
+	return convertView;
     }
 
     static class ViewHolder {
 	ImageView productImage;
-	TextView productName;
-	// Not implemented
-	// int numberOfProducts;
-	LinearLayout extraIngrents;
-	ImageButton trash;
+	TextView productTitle;
+	// Not implemented yet
+	// int numberOfEqualProducts;
+	ImageButton trashIcon;
+	TextView productPrice;
+	LinearLayout extraIngrentsLayout;
     }
 
     // ====================
