7a4b53c5e5ca7110ba44e5930940d841175fba5a
Bernabe Gonzalez <bernagonzga@gmail.com>
2014-05-08 18:11:04 +0200

    Add header license to CustomTextView class

--- ARcowabungaproject/src/org/escoladeltreball/arcowabungaproject/utils/CustomTextView.java
+++ ARcowabungaproject/src/org/escoladeltreball/arcowabungaproject/utils/CustomTextView.java
@@ -1,3 +1,27 @@
+/*
+ *  CustomTextView.java
+ *  
+ *  This file is part of ARcowabungaproject.
+ *  
+ *  Copyright 2014 	Bernabe Gonzalez Garcia <bernagonzga@gmail.com>
+ *  			Marc Sabate Piñol <masapim@hotmail.com>
+ *  			Victor Purcallas Marchesi <vpurcallas@gmail.com>
+ *  			Joaquim Dalmau Torva <jdalmaut@gmail.com>
+ *
+ *   ARcowabungaproject is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   ARcowabungaproject is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with ARcowabungaproject.  If not, see <http://www.gnu.org/licenses/>. 
+ */
+
 package org.escoladeltreball.arcowabungaproject.utils;
 
 import android.content.Context;
