4bd4082ecf6741a8873706696f53a57e1a735c8a
Bernabe Gonzalez <bernagonzga@gmail.com>
2014-05-08 18:10:34 +0200

    Add header license to DAOAndroid, DataBaseHelper class

--- ARcowabungaproject/src/org/escoladeltreball/arcowabungaproject/dao/DataBaseHelper.java
+++ ARcowabungaproject/src/org/escoladeltreball/arcowabungaproject/dao/DataBaseHelper.java
@@ -1,3 +1,27 @@
+/*
+ *  DataBaseHelper.java
+ *  
+ *  This file is part of ARcowabungaproject.
+ *  
+ *  Copyright 2014 	Bernabe Gonzalez Garcia <bernagonzga@gmail.com>
+ *  			Marc Sabate Piñol <masapim@hotmail.com>
+ *  			Victor Purcallas Marchesi <vpurcallas@gmail.com>
+ *  			Joaquim Dalmau Torva <jdalmaut@gmail.com>
+ *
+ *   ARcowabungaproject is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   ARcowabungaproject is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with ARcowabungaproject.  If not, see <http://www.gnu.org/licenses/>. 
+ */
+
 package org.escoladeltreball.arcowabungaproject.dao;
 
 import org.escoladeltreball.arcowabungaproject.model.dao.DAOFactory;
@@ -6,30 +30,30 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-public class DataBaseHelper extends SQLiteOpenHelper{
-    //====================
+public class DataBaseHelper extends SQLiteOpenHelper {
+    // ====================
     // CONSTANTS
-    //====================
+    // ====================
 
-    private static final String DATABASE_NAME ="cowabunga.db";
+    private static final String DATABASE_NAME = "cowabunga.db";
     private static final int DATABASE_VERSION = 1;
-    
-    //====================
-    // ATTRIBUTES
-    //====================
 
-    //====================
+    // ====================
+    // ATTRIBUTES
+    // ====================
+
+    // ====================
     // CONSTRUCTORS
-    //====================
+    // ====================
 
     public DataBaseHelper(Context context) {
 	super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
-    //====================
+    // ====================
     // PUBLIC METHODS
-    //====================
-    
+    // ====================
+
     @Override
     public void onCreate(SQLiteDatabase db) {
 	db.execSQL(DAOFactory.CREATE_TABLE_ADDRESS);
@@ -44,7 +68,7 @@ public class DataBaseHelper extends SQLiteOpenHelper{
 	db.execSQL(DAOFactory.CREATE_TABLE_RESOURCES);
 	db.execSQL(DAOFactory.CREATE_TABLE_SHOPPINCART_PRODUCTS);
 	db.execSQL(DAOFactory.CREATE_TABLE_SHOPPINGCARTS);
-	
+
     }
 
     @Override
@@ -61,24 +85,22 @@ public class DataBaseHelper extends SQLiteOpenHelper{
 	db.execSQL(DAOFactory.DROP_TABLE_RESOURCES);
 	db.execSQL(DAOFactory.DROP_TABLE_SHOPPINGCARTS);
 	db.execSQL(DAOFactory.DROP_TABLE_SHOPPINGCARTS_PRODUCTS);
-	
+
     }
 
-   
-   
-    //====================
+    // ====================
     // PROTECTED METHODS
-    //====================
+    // ====================
 
-    //====================
+    // ====================
     // PRIVATE METHODS
-    //====================
+    // ====================
 
-    //====================
+    // ====================
     // OVERRIDE METHODS
-    //====================
+    // ====================
 
-    //====================
+    // ====================
     // GETTERS & SETTERS
-    //====================
+    // ====================
 }
