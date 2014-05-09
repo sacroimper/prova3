591e726869e5e46754ac8e696b5f12f2eeb6d4fd
Bernabe Gonzalez <bernagonzga@gmail.com>
2014-05-09 17:18:15 +0200

    Add new listener class

---
+++ ARcowabungaproject/src/org/escoladeltreball/arcowabungaproject/listeners/AddButtonClickListener.java
@@ -0,0 +1,83 @@
+/*
+ *  AddButtonClickListener.java
+ *  
+ *  This file is part of ARcowabungaproject.
+ *  
+ *  Copyright 2014 	Bernabe Gonzalez Garcia <bernagonzga@gmail.com>
+ *  			Marc Sabate Piñol <masapim@hotmail.com>
+ *  			Victor Purcallas Marchesi <vpurcallas@gmail.com>
+ *  			Joaquim Dalmau Torva <jdalmaut@gmail.com>
+ *
+ *   ARcowabungaproject is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   ARcowabungaproject is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with ARcowabungaproject.  If not, see <http://www.gnu.org/licenses/>. 
+ */
+package org.escoladeltreball.arcowabungaproject.listeners;
+
+import org.escoladeltreball.arcowabungaproject.model.Product;
+import org.escoladeltreball.arcowabungaproject.model.system.Pizzeria;
+
+import android.app.Activity;
+import android.view.View;
+import android.widget.Toast;
+
+/**
+ * @author local
+ * 
+ */
+public class AddButtonClickListener implements
+	android.view.View.OnClickListener {
+
+    // ====================
+    // CONSTANTS
+    // ====================
+
+    // ====================
+    // ATTRIBUTES
+    // ====================
+    private Product product;
+    private Activity activity;
+
+    // ====================
+    // CONSTRUCTORS
+    // ====================
+    public AddButtonClickListener(Product product, Activity activity) {
+	this.product = product;
+	this.activity = activity;
+    }
+
+    // ====================
+    // PUBLIC METHODS
+    // ====================
+    @Override
+    public void onClick(View v) {
+	Pizzeria.getInstance().getShoppingCart().addProduct(product);
+	Toast.makeText(activity,
+		"S'ha afegit una producte al carro de la compra",
+		Toast.LENGTH_SHORT).show();
+    }
+    // ====================
+    // PROTECTED METHODS
+    // ====================
+
+    // ====================
+    // PRIVATE METHODS
+    // ====================
+
+    // ====================
+    // OVERRIDE METHODS
+    // ====================
+
+    // ====================
+    // GETTERS & SETTERS
+    // ====================
+}
