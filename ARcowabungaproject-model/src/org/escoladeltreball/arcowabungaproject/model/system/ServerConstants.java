0a4443e7cb3b97d6b33100f60429d0ff89efbcce
Marc Sabate <masapim@hotmail.com>
2014-05-14 18:36:29 +0200

    Add ServerConstants class to model

---
+++ ARcowabungaproject-model/src/org/escoladeltreball/arcowabungaproject/model/system/ServerConstants.java
@@ -0,0 +1,63 @@
+/*
+ *  ServerConstants.java
+ *  
+ *  This file is part of ARcowabungaproject.
+ *  
+ *  Copyright 2014 	Bernabe Gonzalez Garcia <bernagonzga@gmail.com>
+ *  			Marc Sabate Piñol <masapim@hotmail.com>
+ *  			Victor Purcallas Marchesi <vpurcallas@gmail.com>
+ *  			Joaquim Dalmau Torva <jdalmaut@gmail.com>
+ *
+ *   ARcowabungaproject is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   ARcowabungaproject is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with ARcowabungaproject.  If not, see <http://www.gnu.org/licenses/>. 
+ */
+
+package org.escoladeltreball.arcowabungaproject.model.system;
+
+public class ServerConstants {
+
+    // ====================
+    // CONSTANTS
+    // ====================
+
+    public static final int SERVER_OPTION_DATABASE_UPDATE = 1;
+    public static final int SERVER_OPTION_SEND_ORDER = 2;
+
+    // ====================
+    // ATTRIBUTES
+    // ====================
+
+    // ====================
+    // CONSTRUCTORS
+    // ====================
+
+    // ====================
+    // PUBLIC METHODS
+    // ====================
+
+    // ====================
+    // PROTECTED METHODS
+    // ====================
+
+    // ====================
+    // PRIVATE METHODS
+    // ====================
+
+    // ====================
+    // OVERRIDE METHODS
+    // ====================
+
+    // ====================
+    // GETTERS & SETTERS
+    // ====================
+}
