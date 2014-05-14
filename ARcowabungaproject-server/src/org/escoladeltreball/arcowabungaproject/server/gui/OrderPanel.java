5ddab325bf73c928b0520cc2accb1591aa67fdb2
Joaquim Dalmau Torvà <jdalmaut@gmail.com>
2014-05-14 21:20:11 +0200

    Add OrderPanel.java in Arcowabungaproject-server

---
+++ ARcowabungaproject-server/src/org/escoladeltreball/arcowabungaproject/server/gui/OrderPanel.java
@@ -0,0 +1,74 @@
+/*
+ *  OrderPanel.java
+ *  
+ *  This file is part of ARcowabungaproject.
+ *  
+ *  Copyright 2014 	Bernabe Gonzalez Garcia <bernagonzga@gmail.com>
+ *  			Marc Sabate Piñol <masapim@hotmail.com>
+ *  			Victor Purcallas Marchesi <vpurcallas@gmail.com>
+ *  			Joaquim Dalmau Torva <jdalmaut@gmail.com>
+ *
+ *   ARcowabungaproject is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   ARcowabungaproject is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with ARcowabungaproject.  If not, see <http://www.gnu.org/licenses/>. 
+ */
+package org.escoladeltreball.arcowabungaproject.server.gui;
+
+import javax.swing.JPanel;
+
+import org.escoladeltreball.arcowabungaproject.model.Order;
+
+public class OrderPanel extends JPanel {
+
+    private Order order;
+
+    public OrderPanel(Order order) {
+	this.order = order;
+	this.initComponents();
+    }
+
+    private void initComponents() {
+
+    }
+
+    // ====================
+    // CONSTANTS
+    // ====================
+
+    // ====================
+    // ATTRIBUTES
+    // ====================
+
+    // ====================
+    // CONSTRUCTORS
+    // ====================
+
+    // ====================
+    // PUBLIC METHODS
+    // ====================
+
+    // ====================
+    // PROTECTED METHODS
+    // ====================
+
+    // ====================
+    // PRIVATE METHODS
+    // ====================
+
+    // ====================
+    // OVERRIDE METHODS
+    // ====================
+
+    // ====================
+    // GETTERS & SETTERS
+    // ====================
+}
