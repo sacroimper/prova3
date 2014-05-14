b954b2aabd13eaed4030b16c4af2b7b88725d0df
Marc Sabate <masapim@hotmail.com>
2014-05-14 18:28:08 +0200

    Add unimplemented DAOPostgreSQL class

---
+++ ARcowabungaproject-server/src/org/escoladeltreball/arcowabungaproject/server/dao/DAOPostgreSQL.java
@@ -0,0 +1,181 @@
+/*
+ *  DAOPostgreSQL.java
+ *  
+ *  This file is part of ARcowabungaproject.
+ *  
+ *  Copyright 2014 	Bernabe Gonzalez Garcia <bernagonzga@gmail.com>
+ *  			Marc Sabate Piñol <masapim@hotmail.com>
+ *  			Victor Purcallas Marchesi <vpurcallas@gmail.com>
+ *  			Joaquim Dalmau Torva <jdalmaut@gmail.com>
+ *
+ *   ARcowabungaproject is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   ARcowabungaproject is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with ARcowabungaproject.  If not, see <http://www.gnu.org/licenses/>. 
+ */
+
+package org.escoladeltreball.arcowabungaproject.server.dao;
+
+import java.util.Map;
+import java.util.Set;
+
+import org.escoladeltreball.arcowabungaproject.model.Address;
+import org.escoladeltreball.arcowabungaproject.model.Drink;
+import org.escoladeltreball.arcowabungaproject.model.Ingredient;
+import org.escoladeltreball.arcowabungaproject.model.Offer;
+import org.escoladeltreball.arcowabungaproject.model.Order;
+import org.escoladeltreball.arcowabungaproject.model.Pizza;
+import org.escoladeltreball.arcowabungaproject.model.Product;
+import org.escoladeltreball.arcowabungaproject.model.ShoppingCart;
+import org.escoladeltreball.arcowabungaproject.model.dao.DAOFactory;
+
+public class DAOPostgreSQL extends DAOFactory {
+
+    // ====================
+    // CONSTANTS
+    // ====================
+
+    // ====================
+    // ATTRIBUTES
+    // ====================
+
+    // ====================
+    // CONSTRUCTORS
+    // ====================
+
+    // ====================
+    // PUBLIC METHODS
+    // ====================
+
+    // ====================
+    // PROTECTED METHODS
+    // ====================
+
+    // ====================
+    // PRIVATE METHODS
+    // ====================
+
+    // ====================
+    // OVERRIDE METHODS
+    // ====================
+
+    @Override
+    protected Set<Product> readProducts() {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    @Override
+    protected Set<Ingredient> readIngredient() {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    @Override
+    protected Set<Pizza> readPizza() {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    @Override
+    protected Set<Offer> readOffer() {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    @Override
+    protected Set<Drink> readDrink() {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    @Override
+    protected ShoppingCart readShoppingCart(int idShoppingCart) {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    @Override
+    protected Set<Order> readOrder() {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    @Override
+    protected Address readAddress(int idAddress) {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    @Override
+    protected Map<String, String> readPreferences() {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    @Override
+    protected void writeProduct(int idProduct) {
+	// TODO Auto-generated method stub
+
+    }
+
+    @Override
+    protected void writeIngredients(Set<Ingredient> ingredients) {
+	// TODO Auto-generated method stub
+
+    }
+
+    @Override
+    protected void writePizzas(Set<Pizza> pizzas) {
+	// TODO Auto-generated method stub
+
+    }
+
+    @Override
+    protected void writeOffers(Set<Offer> offers) {
+	// TODO Auto-generated method stub
+
+    }
+
+    @Override
+    protected void writeDrinks(Set<Drink> drinks) {
+	// TODO Auto-generated method stub
+
+    }
+
+    @Override
+    protected void writeShoppingCarts(ShoppingCart shoppingCart) {
+	// TODO Auto-generated method stub
+
+    }
+
+    @Override
+    protected void writeOrders(Set<Order> orders) {
+	// TODO Auto-generated method stub
+
+    }
+
+    @Override
+    protected void writeAddresses(Address address) {
+	// TODO Auto-generated method stub
+
+    }
+
+    @Override
+    protected void writePreferences(Map<String, String> preferences) {
+	// TODO Auto-generated method stub
+
+    }
+
+    // ====================
+    // GETTERS & SETTERS
+    // ====================
+}
