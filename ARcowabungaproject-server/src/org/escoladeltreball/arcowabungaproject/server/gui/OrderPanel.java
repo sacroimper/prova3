e2373c4940cf5f3f1c025ed96c96f08845c8f14d
Joaquim Dalmau Torvà <jdalmaut@gmail.com>
2014-05-15 10:42:54 +0200

    Implement OrderPanel.java

--- ARcowabungaproject-server/src/org/escoladeltreball/arcowabungaproject/server/gui/OrderPanel.java
+++ ARcowabungaproject-server/src/org/escoladeltreball/arcowabungaproject/server/gui/OrderPanel.java
@@ -23,23 +23,25 @@
  */
 package org.escoladeltreball.arcowabungaproject.server.gui;
 
-import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.util.ArrayList;
 
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.Border;
+
+import org.escoladeltreball.arcowabungaproject.model.Drink;
+import org.escoladeltreball.arcowabungaproject.model.Offer;
 import org.escoladeltreball.arcowabungaproject.model.Order;
+import org.escoladeltreball.arcowabungaproject.model.Pizza;
+import org.escoladeltreball.arcowabungaproject.model.Product;
 
 public class OrderPanel extends JPanel {
-
-    private Order order;
-
-    public OrderPanel(Order order) {
-	this.order = order;
-	this.initComponents();
-    }
-
-    private void initComponents() {
-
-    }
-
     // ====================
     // CONSTANTS
     // ====================
@@ -48,9 +50,23 @@ public class OrderPanel extends JPanel {
     // ATTRIBUTES
     // ====================
 
+    private Order order;
+    private Border border;
+    private JLabel jlOrderId;
+    private JLabel jlNumOfPizzas;
+    private JLabel jlNumOfDrinks;
+    private JLabel jlNumOfOffers;
+    private JLabel jlTotalPrice;
+    private JButton jbMakePizza;
+    private JButton jbShowInfo;
+
     // ====================
     // CONSTRUCTORS
     // ====================
+    public OrderPanel(Order order) {
+	this.order = order;
+	this.initComponents();
+    }
 
     // ====================
     // PUBLIC METHODS
@@ -63,6 +79,94 @@ public class OrderPanel extends JPanel {
     // ====================
     // PRIVATE METHODS
     // ====================
+
+    private void initComponents() {
+	this.border = BorderFactory.createBevelBorder(BevelBorder.RAISED,
+		new Color(205, 9, 9), new Color(232, 68, 68));
+	this.setLayout(new GridBagLayout());
+
+	// Put information in components
+	this.jlOrderId = new JLabel("ID: " + this.order.getId());
+	this.jlNumOfPizzas = new JLabel("Pizzas: "
+		+ this.numOfDifferentsProductsInOrder()[0]);
+	this.jlNumOfDrinks = new JLabel("Drinks: "
+		+ this.numOfDifferentsProductsInOrder()[1]);
+	this.jlNumOfOffers = new JLabel("Offers: "
+		+ this.numOfDifferentsProductsInOrder()[2]);
+	this.jlTotalPrice = new JLabel("Total Price: "
+		+ this.order.getShoppingCart().getPrice() + "€");
+
+	this.jbMakePizza = new JButton("Make Pizza!");
+	this.jbShowInfo = new JButton("Show More Info");
+
+	GridBagConstraints constraints = new GridBagConstraints();
+	constraints.gridx = 0;
+	constraints.gridy = 0;
+	constraints.gridheight = 3;
+	this.add(this.jlOrderId, constraints);
+	constraints.gridx = 1;
+	constraints.gridy = 0;
+	constraints.gridheight = 1;
+	this.add(this.jlNumOfPizzas, constraints);
+	constraints.gridx = 1;
+	constraints.gridy = 1;
+	this.add(this.jlNumOfDrinks, constraints);
+	constraints.gridx = 1;
+	constraints.gridy = 2;
+	this.add(this.jlNumOfOffers, constraints);
+	constraints.gridx = 2;
+	constraints.gridy = 0;
+	constraints.gridheight = 3;
+	this.add(this.jlTotalPrice, constraints);
+	constraints.gridx = 3;
+	constraints.gridy = 0;
+	constraints.gridheight = 3;
+	this.add(this.jbMakePizza, constraints);
+	constraints.gridx = 4;
+	constraints.gridy = 0;
+	constraints.gridheight = 3;
+	this.add(this.jbShowInfo, constraints);
+    }
+
+    /**
+     * Calculate the number of different products in the order.
+     * 
+     * @return an array where the number of pizzas are stored in index 0, the
+     *         number of drinks are stored in index 1 and the number of offers
+     *         are stored in index 2
+     */
+    private int[] numOfDifferentsProductsInOrder() {
+	int numOfPizzas = 0;
+	int numOfDriks = 0;
+	int numOfOffers = 0;
+	int[] differentProducts = new int[3];
+	ArrayList<Product> products = (ArrayList<Product>) this.order
+		.getShoppingCart().getProducts();
+	for (Product product : products) {
+	    if (product instanceof Pizza) {
+		numOfPizzas++;
+	    } else if (product instanceof Drink) {
+		numOfDriks++;
+	    } else if (product instanceof Offer) {
+		numOfOffers++;
+		Offer offer = (Offer) product;
+		ArrayList<Product> offerProducts = (ArrayList<Product>) offer
+			.getProductList();
+		for (Product offerProduct : offerProducts) {
+		    if (offerProduct instanceof Pizza) {
+			numOfPizzas++;
+		    } else if (offerProduct instanceof Drink) {
+			numOfDriks++;
+		    }
+
+		}
+	    }
+	}
+	differentProducts[0] = numOfPizzas;
+	differentProducts[1] = numOfDriks;
+	differentProducts[2] = numOfOffers;
+	return differentProducts;
+    }
 
     // ====================
     // OVERRIDE METHODS
